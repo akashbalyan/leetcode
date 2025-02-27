2812a319ad1b8794158ff76ff29c4eab66cf3039
Akash Balyan <40184394+akashbalyan@users.noreply.github.com>
2025-02-27 08:55:58 -0800

    Time: 3 ms (83.55%) | Memory: 46 MB (42.70%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int candy(int[] ratings) {
+
+     int arr [] = new int [ratings.length];
+    //Arrays.fill(arr,1);
+     for(int i=0;i<arr.length;i++){
+        arr[i] = 1;
+     }  
+
+     for(int i=1;i<arr.length;i++){
+        if(ratings[i]>ratings[i-1]){
+            arr[i] = arr[i-1] +1;
+        }
+     }
+     for(int i=arr.length-2;i>=0;i--){
+        if(ratings[i]>ratings[i+1] && !(arr[i]>arr[i+1])){
+            arr[i] = arr[i+1] +1;
+        }
+     }
+     int sum = 0;
+     for(int i:arr){
+        sum = sum +i;
+     }
+
+     return sum;
+    }
+}
